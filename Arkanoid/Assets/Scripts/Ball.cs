6719e06a408bdaaba40f1971b95a1ddfcd8f5b5f
Protosaider <workinghandguard@gmail.com>
2017-06-25 15:20:34 +0300

    First commit

---
+++ Arkanoid/Assets/Scripts/Ball.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    public Vector3 startPosition;
+    public float initialVelocity = 600f;
+    private Rigidbody rb;
+    //private Vector3 push;
+
+    private bool ballInGame = false;
+	
+	void Awake() 
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update() 
+    {
+        Debug.Log("Velocity: " + rb.velocity + " angular: " + rb.angularVelocity);
+        //need to call GetButtonDown from the Update function
+        //if (Input.GetKeyDown(KeyCode.Space) && ballInGame == false)
+        if (Input.GetButtonDown("Jump") && ballInGame == false)
+        {
+            transform.parent = null;
+            ballInGame = true;
+            //whether physics affects rigidbody
+            rb.isKinematic = false;
+            float x = Mathf.Clamp(Random.Range(-1.0f, 1.0f), -0.8f, 0.8f);
+            Vector3 push = new Vector3(x, 0, 1.0f - Mathf.Abs(x)) * initialVelocity;
+            rb.AddForce(push);        
+            //rb.velocity = Vector3.left;
+        } else 
+            if (Input.GetButtonDown("Jump") && ballInGame == true)
+            {
+                transform.parent = null;
+                ballInGame = false;
+                //whether physics affects rigidbody
+                rb.isKinematic = false;
+                transform.position = startPosition;
+                //rb.AddForce(-push);
+                rb.velocity = Vector3.zero;
+                rb.angularVelocity = Vector3.zero;
+            }
+	}
+}
---
+++ Arkanoid/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private Rigidbody rb;
+    public float playerSpeed;
+    public Vector3 startPosition;
+	// Use this for initialization
+	void Start () 
+    {
+        //rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		
+	}
+
+    void FixedUpdate()
+    {
+        //Debug.Log(Mathf.Clamp(7, 1, 5));
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        //Clamp - to make a border for platform.
+        //transform.position = new Vector3(Mathf.Clamp(transform.position.x + (moveHorizontal * playerSpeed), -22.4f, 22.4f), transform.position.y, transform.position.z);
+        transform.position = new Vector3(transform.position.x + (moveHorizontal * playerSpeed), transform.position.y, transform.position.z);
+        //Vector3 movement = new Vector3(moveHorizontal, 0.0f);
+        //rb.AddForce(movement * playerSpeed, ForceMode.Force);
+        /*if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            transform.position = startPosition;
+        }*/
+    }
+}
