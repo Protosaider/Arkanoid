84659a0863813cdf009cf9d7c323790a9ce15d42
Protosaider <workinghandguard@gmail.com>
2017-06-26 04:11:56 +0300

    At least not sooo bad. Little bugfix. ___Kluge strange thing with resolution.___ Include build version for Win x64; Tested resolution: Native 1600x900.

---
+++ Assets/Scripts/Floor.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : MonoBehaviour {
+
+    void OnTriggerEnter(Collider collider)
+    {
+        GameManager.instance.LoseLife();
+    }
+}
